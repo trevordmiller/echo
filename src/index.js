392cfac840582cbba05469068c51a903165ec1ed
Colin Pirillo <colinpirillo@gmail.com>
2016-10-07 22:00:31 -0400

    redo import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import Home from './components/Home/Home'
-import * as errors from './utils/errors.js'
+import errors from './utils/errors.js'
 
 errors();
 
