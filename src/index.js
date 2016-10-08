e50e54878e1d13e64b554d1030448d2ee2f2da15
Colin Pirillo <colinpirillo@gmail.com>
2016-10-07 21:16:45 -0400

    import and set up raven

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import Home from './components/Home/Home'
+import * as errors from './utils/errors.js'
+
+errors();
 
 ReactDOM.render(
   <App><Home /></App>,
